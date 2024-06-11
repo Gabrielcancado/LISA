34000e383669b78e5e76ca4f67644c8f8c4a5375
Gabrielcancado <102363667+Gabrielcancado@users.noreply.github.com>
2024-06-11 19:53:56 -0300

    distancia_pub.py

--- distancia_pub.py
+++ distancia_pub.py
@@ -8,20 +8,18 @@ import numpy as np
 from std_msgs.msg import Float32
 import mediapipe as mp
 
-# Função para calcular a distância do rosto até a câmera
 def calcular_distancia(face_width, focal_length, pixel_width):
     return (face_width * focal_length) / pixel_width
 
-# Parâmetros de calibração da câmera
+# Matriz de calibração da minha câmera
 camera_matrix = np.array([[6.73013110e+02, 0.00000000e+00, 1.74499999e+02],
                           [0.00000000e+00, 6.72565563e+02, 1.49500000e+02],
                           [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
-# Coeficientes de distorção
+# Coeficientes de distorção da minha câmera
 dist_coeffs = np.array([[-1.20362885e-01, 4.78388299e-02, -2.92259372e-03, -6.27378830e-03, 1.16195179e-02]])
 
-# Largura média do rosto humano em centímetros
-average_face_width = 14.0  # cm (aproximadamente)
+average_face_width = 14.0 
 
 class DistancePublisher:
     def __init__(self):
@@ -33,7 +31,6 @@ class DistancePublisher:
 
     def image_callback(self, data):
         try:
-            # Verifique o tipo de encoding da imagem
             if data.encoding == "8UC3":
                 rospy.logwarn(f"Unexpected image encoding: {data.encoding}. Converting to 'bgr8'.")
                 # Converte a mensagem ROS para uma imagem OpenCV com tipo '8UC3'
@@ -54,20 +51,20 @@ class DistancePublisher:
                     face_width_in_pixels = w  # Largura do rosto em pixels
                     distance = calcular_distancia(average_face_width, focal_length, face_width_in_pixels)
 
+                    # Nessa função eu coloquei para ele publicar apenas a menor distância, testei e deu certo
                     if distance < min_distance and distance <= 100:  # Considerar apenas distâncias até 1 metro
                         min_distance = distance
 
-                # Publica a menor distância calculada se estiver dentro do limite
+                # Defini a distância máxima para ele publicar como sendo 100cm
                 if min_distance <= 100:
                     self.distance_pub.publish(min_distance)
         except Exception as e:
             rospy.logerr(f"Erro ao processar a imagem: {e}")
 
     def detect_faces(self, image):
-        # Converte a imagem para RGB (MediaPipe trabalha com RGB)
+        # Converte a imagem para RGB pra usar o mediapipe
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Processa a imagem para detectar rostos
         results = self.face_detection.process(rgb_image)
 
         faces = []
